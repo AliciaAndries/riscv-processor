c172da57d9400c6f339204d62280feb9ca03acfc
alicia.andries <alicia.andries@student.kuleuven.be>
2021-10-29 16:06:55 +0200

    Immgen test works

--- src/test/scala/ImmGen_test.scala
+++ src/test/scala/ImmGen_test.scala
@@ -20,7 +20,7 @@ class ImmGenTester extends BasicTester {
 
     val inst = VecInit(insts)(cntr)
     dut.io.inst := inst
-    val immGenCtrl = compare(9,7)
+    val immGenCtrl = compare(14,12)
     dut.io.immGenCtrl := immGenCtrl
 
     val out = dut.io.out
@@ -37,6 +37,8 @@ class ImmGenTester extends BasicTester {
     val immgeni = Cat(Cat(Seq.fill(21){inst31}), inst30_25, inst24_21, inst20) 
     val immgens = Cat(Cat(Seq.fill(21){inst31}), inst30_25, inst11_8, inst7)
     val immgenb = Cat(Cat(Seq.fill(20){inst31}), inst7, inst30_25, inst11_8, 0.U)
+    val immgenu = Cat(inst31, inst30_25, inst24_21, inst20, inst19_12, Cat(Seq.fill(12)(0.U)))
+    val immgenj = Cat(Cat(Seq.fill(12)(inst31)), inst19_12, inst20, inst30_25, inst24_21, 0.U)
     /*
     ...
     */
@@ -46,14 +48,16 @@ class ImmGenTester extends BasicTester {
       Seq(
         ImmGen.I -> immgeni,
         ImmGen.S -> immgens,
-        ImmGen.B -> immgenb
+        ImmGen.B -> immgenb,
+        ImmGen.U -> immgenu,
+        ImmGen.J -> immgenj
       )
     )
     // printf("b31 = %d, b30to20 = %d, b19to12 = %d, b11 = %d, b10to5 = %d, b4to1 = %d, b0 = %d\n"
     //       , dut.io.test.b31, dut.io.test.b30to20, dut.io.test.b19to12, dut.io.test.b11, dut.io.test.b10to5, dut.io.test.b4to1, dut.io.test.b0)
     printf("inst31 = %d, inst30_25 = %d, inst24_21 = %d, inst20 = %d, inst19_12 = %d, inst11_8 = %d, inst7 = %d\n"
           , inst31, inst30_25, inst24_21, inst20, inst19_12, inst11_8, inst7)
-    printf("cntr = %d, extended = %d, check = %d, 12 bits = %d\n", cntr, out, out_check, inst(31,20).asSInt)
+    printf("cntr = %d, cntrl = %d, extended = %d, check = %d, 12 bits = %d\n", cntr, immGenCtrl, out, out_check, inst(31,20).asSInt)
     when(immGenCtrl.orR){
       assert(out === out_check)
     }
